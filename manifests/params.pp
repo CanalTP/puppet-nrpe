cb13c2565dbb33faa0ec2fc2b5ab6bec078d1ff7
Rudy YAYON <ryayon@outlook.com>
2016-03-09 10:23:22 +0100

    params.pp was not formalized correctly

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,12 +2,12 @@ class nrpe::params {
 
   if $::osfamily == 'RedHat' {
 
-    packages = [ 'nrpe', 'nagios-plugins-all' ]
-    config_nrpe_user = 'nrpe'
-    config_nrpe_group = 'nrpe'
-    config_include_dir = '/etc/nrpe.d'
-    config_plugins_dir = '/usr/lib64/nagios/plugins'
-    service_name = 'nrpe'
+    $packages           = ['nrpe', 'nagios-plugins-all']
+    $config_nrpe_user   = 'nrpe'
+    $config_nrpe_group  = 'nrpe'
+    $config_include_dir = '/etc/nrpe.d'
+    $config_plugins_dir = '/usr/lib64/nagios/plugins'
+    $service_name       = 'nrpe'
 
     if $::operatingsystemmajrelease == 6 {
     }
@@ -16,19 +16,20 @@ class nrpe::params {
   }
   elsif $::osfamily == 'Debian' {
 
-    packages = [ 'nagios-nrpe-server', 'nagios-plugins' ]
-    config_nrpe_user = 'nagios'
-    config_nrpe_group = 'nagios'
-    config_include_dir = '/etc/nagios/nrpe.d'
-    config_plugins_dir = '/usr/lib/nagios/plugins'
-    service_name = 'nagios-nrpe-server'
+    $packages           = ['nagios-nrpe-server', 'nagios-plugins']
+    $config_nrpe_user   = 'nagios'
+    $config_nrpe_group  = 'nagios'
+    $config_include_dir = '/etc/nagios/nrpe.d'
+    $service_name       = 'nagios-nrpe-server'
 
     if $::operatingsystemmajrelease == 6 {
-      config_plugins_dir = '/usr/lib64/nagios/plugins'
+      $config_plugins_dir = '/usr/lib64/nagios/plugins'
     }
     elsif $::operatingsystemmajrelease == 7 {
+      $config_plugins_dir = '/usr/lib/nagios/plugins'
     }
     elsif $::operatingsystemmajrelease == 8 {
+      $config_plugins_dir = '/usr/lib/nagios/plugins'
     }
   }
 
