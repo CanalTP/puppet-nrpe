511781d4aedb60d9b0933a043e331e5740fb5790
Rudy YAYON <ryayon@outlook.com>
2016-03-09 11:29:15 +0100

    params rspec added

---
+++ spec/classes/params_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'nrpe::params', :type => :class do
+  context "On a RedHat OS" do
+    let :facts do
+      {
+        :osfamily => 'RedHat',
+        :operatingsystemmajrelease => '7',
+      }
+    end
+    it { is_expected.to contain_nrpe__params }
+
+    it "Should not contain any resources" do
+      should have_resource_count(0)
+    end
+  end
+end
