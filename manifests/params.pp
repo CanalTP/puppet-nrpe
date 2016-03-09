dda83e164de4f7be537e5ba4ccec18277631141e
Rudy YAYON <ryayon@outlook.com>
2016-03-09 13:07:24 +0100

    Missing / in configuration include_dir parameter

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class nrpe::params {
       $package_name       = ['nagios-nrpe-server', 'nagios-plugins']
       $config_nrpe_user   = 'nagios'
       $config_nrpe_group  = 'nagios'
-      $config_include_dir = '/etc/nagios/nrpe.d'
+      $config_include_dir = '/etc/nagios/nrpe.d/'
       $service_name       = 'nagios-nrpe-server'
 
       case $::operatingsystemmajrelease {
@@ -26,7 +26,7 @@ class nrpe::params {
       $package_name       = ['nrpe', 'nagios-plugins-all']
       $config_nrpe_user   = 'nrpe'
       $config_nrpe_group  = 'nrpe'
-      $config_include_dir = '/etc/nrpe.d'
+      $config_include_dir = '/etc/nrpe.d/'
       $config_plugins_dir = '/usr/lib64/nagios/plugins'
       $service_name       = 'nrpe'
 
