ddf2669eac3131da7b44f45a5c1a288529244aeb
Rudy YAYON <ryayon@outlook.com>
2016-07-01 13:51:28 +0200

    NRPE allowed hosts hardcoded

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class nrpe (
   $package_ensure       = installed,
   $config_nrpe_user     = $::nrpe::params::config_nrpe_user,
   $config_nrpe_group    = $::nrpe::params::config_nrpe_group,
-  $config_allowed_hosts = undef,
+  $config_allowed_hosts = [ 'ctp-prd-poller.canaltp.prod', 'ctp-prd-poller2.canaltp.prod', 'ctp-prd-poller3.canaltp.prod', 'par-vm03.srv.canaltp.fr', '10.2.0.250' ],
   $config_include_dir   = $::nrpe::params::config_include_dir,
   $config_plugins_dir   = $::nrpe::params::config_plugins_dir,
   $service_name         = $::nrpe::params::service_name,
