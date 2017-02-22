89d8f7422b2c1ed12b698c90fb0833e44191883c
Mady Camara <mady.camara@canaltp.fr>
2017-02-22 17:40:13 +0100

    hotfix

--- files/plugins/contrib/status_mongo_backup.sh
+++ files/plugins/contrib/status_mongo_backup.sh
@@ -7,10 +7,9 @@ MONITOR_FILE=$BACKUP_DIR/supervision.txt
 function check_if_backup_was_correct()
 { 
     logger "[$DATE_TIME] Checking if backup mongo was correct"
-    FAILED_LIST=$(cat $MONITOR_FILE |grep -i "KO"|tr -d "\n")
+    cat $MONITOR_FILE |grep -i "KO"
     RET_CODE=$?
-
-
+    FAILED_LIST=$(cat $MONITOR_FILE |grep -i "KO"|tr -d "\n")
 
     if [ $RET_CODE -eq 0 ];then
         echo "[MONGO_BACKUP][$HOSTNAME] Processus failure when backup $FAILED_LIST"
