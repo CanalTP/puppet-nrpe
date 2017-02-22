05badde29ae624ef020eab95807d69a2b98854b1
Mady Camara <mady.camara@canaltp.fr>
2017-02-22 16:39:10 +0100

    Adding check status for backup mongo processus

---
+++ files/plugins/contrib/status_mongo_backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Monitoring if backup mongo is healthy 
+ 
+BACKUP_DIR="/var/backups/$(date +%F)"
+MONITOR_FILE=$BACKUP_DIR/supervision.txt
+
+function check_if_backup_was_correct()
+{ 
+    logger "[$DATE_TIME] Checking if backup mongo was correct"
+    FAILED_LIST=$(cat $MONITOR_FILE |grep -i "KO"|tr -d "\n")
+    RET_CODE=$?
+    if [ $RET_CODE -eq 0 ];then
+        echo "[MONGO_BACKUP][$HOSTNAME] Processus failure when backup $FAILED_LIST"
+        exit 1
+    else
+        echo "[MONGO_BACKUP][$HOSTNAME] Processus backup is good"
+        exit 0
+   fi
+}
+
+#Call functions
+check_if_backup_was_correct
