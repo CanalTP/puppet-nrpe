747a34b69fe7c30763b7621379212870787a9532
Christophe Biguereau <christophe.biguereau@canaltp.fr>
2017-02-24 17:25:51 +0100

    Update restart_tomcat-sum.sh to use for load and wrong datetime on SUM.

--- files/plugins/contrib/restart_tomcat-sum.sh
+++ files/plugins/contrib/restart_tomcat-sum.sh
@@ -5,7 +5,26 @@
 #Initialisation des Variables
 service_status=$1
 nb_retry=$2
+restart_reson=$3 
+
 DEST_MAIL="dt.exploitation@canaltp.fr"
+SUBJECT_MAIL="Redemarrage automatique de Tomcat-SUM sur ${HOSTNAME}"
+
+# Define body message
+case ${restart_reson} in
+  "load")
+    body="Bonjour,
+Load eleve detecte, le redemarrage de TOMCAT-SUM a ete effectue sur le serveur ${HOSTNAME}
+
+Cordialement."
+    ;;
+  "datetime")
+    body="Bonjour
+Les valeur des clés dateHeure dans la réponse json de ${HOSTNAME} n'est pas valide. Restart du serveur.
+
+Cordialement."
+    ;;
+esac
 
 
 if ( [ "$service_status" != "WARNING" ] && [ "$service_status" != "CRITICAL" ] ) || [ "$nb_retry" != "3" ]
@@ -15,7 +34,5 @@ then
 fi
 
 sudo service tomcat7-sum restart
-echo "Bonjour,
-Load eleve detecte, le redemarrage de TOMCAT-SUM a ete effectue sur le serveur ${HOSTNAME}
 
-Cordialement."  | mail -s "Redemarrage automatique de Tomcat-SUM sur ${HOSTNAME}" ${DEST_MAIL}
+echo "${body}"  | mail -s "${SUBJECT_MAIL}" ${DEST_MAIL}
