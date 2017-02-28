dcdc286ee7849ce3743c536a187c0f8ec0f977d7
Hoshiyo <aguyen89@gmail.com>
2017-02-28 09:48:26 +0100

    add warning state fot check_stat_worker.py

--- files/plugins/contrib/check_stat_worker.py
+++ files/plugins/contrib/check_stat_worker.py
@@ -3,19 +3,23 @@
 
 import requests
 
-CRITICAL = 2
-RETURN_CODE = 2
+CRITICAL = 0
+NORMAL = 3
+RETURN_CODE = 0
 
 try:
         r = requests.get('http://nav2-pre-logn1.canaltp.prod:9999/proxy:spark-master:8080/metrics/master/json/')
 
         metrics = r.json()
         nb_worker = metrics['gauges']['master.aliveWorkers']['value']
-        if nb_worker <= CRITICAL:
-                print("Missing workers. Restart them")
+        if nb_worker == CRITICAL:
+                print("CRITICAL: %d worker is running" % nb_worker)
+		RETURN_CODE = 2
+        elif nb_worker > CRITICAL and nb_worker < NORMAL:
+                print("WARNING: Missing workers. Restart them")
+                RETURN_CODE = 1
         else:
-                print("%d workers is running" % nb_worker)
-                RETURN_CODE = 0
+                print("OK: %d workers is running" % nb_worker)
 except Exception, e:
         print("ERROR: {}".format(e))
 finally:
