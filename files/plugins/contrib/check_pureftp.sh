654d01642767c3d4145ce096f30521ed5a1f09ca
Mady Camara <mady.camara@canaltp.fr>
2017-04-27 15:04:43 +0200

    adding new script

---
+++ files/plugins/contrib/check_pureftp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script that's useful for checking pureftpd
+
+ENV_VALUE=$(echo $HOSTNAME|cut -d "-" -f 2)
+TEMP_FILE=$(mktemp)
+
+GET_CONNECTED_NBR=$(pure-mrtginfo|awk 'NR==1')
+GET_UPTIME_FTP=$(pure-mrtginfo|awk 'NR==3'|awk '{print $1}')
+
+
+echo "Connected clients: CONNECTED=$GET_CONNECTED_NBR, Uptime server: UPTIME_HOST_FTP=${GET_UPTIME_FTP}j"
+exit 0
