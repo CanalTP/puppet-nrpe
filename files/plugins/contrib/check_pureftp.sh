b2a155127fc7a5b276901fb67c7871d935661d74
Christophe Biguereau <christophe.biguereau@canaltp.fr>
2017-12-11 15:17:21 +0100

    Remove unused mktemp command

--- files/plugins/contrib/check_pureftp.sh
+++ files/plugins/contrib/check_pureftp.sh
@@ -2,7 +2,6 @@
 # Script that's useful for checking pureftpd
 
 ENV_VALUE=$(echo $HOSTNAME|cut -d "-" -f 2)
-TEMP_FILE=$(mktemp)
 
 GET_CONNECTED_NBR=$(pure-mrtginfo|awk 'NR==1')
 GET_UPTIME_FTP=$(pure-mrtginfo|awk 'NR==3'|awk '{print $1}')
