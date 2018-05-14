7058d9feb499e30c0ea02cd7a9dfcd2fdcd3b6ee
Jean-Baptiste Valteau <Jean-Baptiste Valtetau>
2018-05-14 16:16:00 +0200

    Add Gormungandr sup

--- files/plugins/contrib/check_swarm_service_gormungandr.sh
+++ files/plugins/contrib/check_swarm_service_gormungandr.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-#Monitoring swarm microservice 
+#Monitoring Gormungandr microservice 
  
-ENV_FLAG=$1
 LOG_RISE_AN_ERROR=0
-PROBE_RISE_AN_ERROR=0
 GORMUNGANDR_RISE_AN_ERROR=0
 TRAEFIK_RISE_AN_ERROR=0
 
 LIST_MICROSERVICE="traefik gormungandr"
-MSG_OUTPUT="Scanning microservice report: "
+MSG_OUTPUT="Scanning Gormungdanr microservice report: "
 COMPLETE_NAME=$(echo $ENV_FLAG |tr '[A-Z]' '[a-z]')
 DATE_TIME=$(date +%F_%T)
-
-function check_parameters()
-{
-   logger "[$DATE_TIME] Checking if parameters are corrects"
-   if [ $# -ne 1 ];then
-  	 echo -e "\t \t [SWARM_MICROSERVICE] \033[31m Parameter is incorrect .. We expect 1 as [prod|rec|cus] \033[0m"
-	 exit 1
-   fi
-}
 
 function check_traefik()
 {
@@ -27,10 +16,10 @@ function check_traefik()
     GET_SRV_STARTED=$(docker -H localhost:2375 service ps traefik_traefik |grep "Running")
 
     if [ -z "$GET_SRV_STARTED" ];then
-           TRAEFIK_RISE_AN_ERROR=1
-           MSG_OUTPUT="$MSG_OUTPUT Traefik:[KO]"
-    else
-           MSG_OUTPUT="$MSG_OUTPUT Traefik:[OK]"
+         TRAEFIK_RISE_AN_ERROR=1
+         MSG_OUTPUT="$MSG_OUTPUT Traefik:[KO]"
+  else
+          MSG_OUTPUT="$MSG_OUTPUT Traefik:[OK]"
     fi
 }
 
@@ -48,14 +37,14 @@ function check_gormungandr()
 }
 
 #Call functions
-check_parameters $ENV_FLAG
+#check_parameters $ENV_FLAG
 check_gormungandr
 check_traefik
 
- if [ $GORMUNGANDR_RISE_AN_ERROR -eq 0 ] && [ $TRAEFIK_RISE_AN_ERROR -eq 0 ];then
-	echo "[CLUSTER_SWARM] [MICROSERVICE] $MSG_OUTPUT"
+if [ $GORMUNGANDR_RISE_AN_ERROR -eq 0 ] && [ $TRAEFIK_RISE_AN_ERROR -eq 0 ];then
+	echo "$MSG_OUTPUT"
   	exit 0
- else
-	echo "[CLUSTER_SWARM] [MICROSERVICE] $MSG_OUTPUT"
+else
+	echo "$MSG_OUTPUT"
 	exit 2
- fi
+fi
